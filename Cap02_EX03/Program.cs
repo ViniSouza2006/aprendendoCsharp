3ed7aa6d92c77be79b05a85ccdac8aaede4a15de
VINICIUS SOUZA DO NASCIMENTO <vinicius.snascimen2@senacsp.edu.br>
2024-08-06 17:12:43 -0300

    Cap02_EX3 Concluido

---
+++ Cap02_EX03/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cap02_EX03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float HT, VH, PD, SB, TD, SL;
+
+            Console.WriteLine("Horas trabalhadas ............: ");
+            HT = float.Parse(Console.ReadLine());
+            Console.WriteLine("Valor da hora ............: ");
+            VH = float.Parse(Console.ReadLine());
+            Console.WriteLine("Valor do percentual de desconto .: ");
+            PD = float.Parse(Console.ReadLine());
+
+            SB = HT * VH;
+            TD = (PD / 100) * SB;
+            SL = SB - TD;
+
+            Console.WriteLine();
+            Console.WriteLine("Salario liquido ............: ");
+            Console.WriteLine(SL.ToString("##,##0.00"));
+
+            Console.WriteLine();
+            Console.WriteLine("Tecla <Enter> para encerrar... ");
+            Console.WriteLine();
+        }
+    }
+}
