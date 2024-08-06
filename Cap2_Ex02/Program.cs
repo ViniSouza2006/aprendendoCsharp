d935d87df4862af0ddd04aa89bfdc4cd923ae613
VINICIUS SOUZA DO NASCIMENTO <vinicius.snascimen2@senacsp.edu.br>
2024-08-06 17:04:04 -0300

    Cap02_EX02 Concluido

---
+++ Cap2_Ex02/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cap2_Ex02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, R,;
+
+            Console.WriteLine("Entre 0 1o. valor: ");
+            A = int.Parse(Console.ReadLine());
+
+            R = A + B;
+
+            Console.WriteLine();
+            Console.WriteLine("Resultado = " + R);
+
+            Console.WriteLine();
+            Console.WriteLine("Tecla <Enter> para continuar... ");
+            Console.WriteLine();
+        }
+    }
+}
